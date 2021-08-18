2decc5902474bb2bea8a1a2c8e7ab8831465ec0b
Juan Ruiz <juancaruiz@Juancas-MacBook-Pro.local>
2021-08-17 21:52:30 -0700

    displaying mail list

--- client/src/styles/MailListHeader.js
+++ client/src/styles/MailListHeader.js
@@ -1,5 +1,21 @@
 import styled from "styled-components";
 
-const Container = styled.div``;
+const Container = styled.div`
+  .mailHeader {
+    font-size: 29px;
+    line-height: 38px;
+
+    letter-spacing: -0.02em;
+
+    color: #080708;
+  }
+
+  .mailSubHeader {
+    font-size: 16px;
+    line-height: 22px;
+
+    color: #080708;
+  }
+`;
 
 export default Container;
